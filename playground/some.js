ebb9fca9dd187cb16f6c2dd34fa64cd998eacf10
Chris George <chrisgeorge.9@gmail.com>
2019-11-28 22:03:54 -0800

    day 1 beginner JS

---
+++ playground/some.js
@@ -0,0 +1,8 @@
+const p = document.querySelector('p');
+console.log('im in another file');
+
+const age = 100;
+console.log(p, age);
+const animals = ['hey', 'you', 'me'];
+log(animals);
+let age = 100;
---
+++ playground/types.js
@@ -0,0 +1,17 @@
+// /* eslint-disable */
+// const name = 'chris';
+// const middle = "thomas";
+// const last = `george`;
+
+// const sentence = `she's so cool`
+
+// const smarties = 20;
+// const kids = 3;
+// const smartiesPerKid = smarties / kids;
+
+// const person = {
+//   name: 'Chris',
+//   age: 29,
+// };
+
+const isDrying = false;
